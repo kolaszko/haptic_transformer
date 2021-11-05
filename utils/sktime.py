8204fdb8b2815dd3b46c2c5d604c062e6c341eb7
mbed92 <michalbednarek.e@gmail.com>
2021-11-05 15:31:48 +0100

    changed name

--- utils/sktime.py
+++ utils/sktime.py
@@ -4,7 +4,7 @@ import numpy as np
 from sktime.datatypes._panel._convert import from_3d_numpy_to_nested
 
 
-def measure_inference_time_sktime(model, shape=(160, 6), repetitions=300):
+def measure_inference_time(model, shape=(160, 6), repetitions=300):
     mock_x = np.zeros(shape=[1, shape[0], shape[1]])
     mock_x = from_3d_numpy_to_nested(mock_x.transpose((0, 2, 1)))
 
