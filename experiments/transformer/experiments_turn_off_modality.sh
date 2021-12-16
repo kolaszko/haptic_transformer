cdfde5479e92559a51dba0c7dc9f74fc05266525
mbed92 <michalbednarek.e@gmail.com>
2021-12-16 14:28:02 +0100

    [bugfix] improved bash scripts

--- experiments/transformer/experiments_turn_off_modality.sh
+++ experiments/transformer/experiments_turn_off_modality.sh
@@ -6,7 +6,5 @@ EPOCHS=$3
 
 export PYTHONPATH=$PYTHONPATH:/home/mbed/Projects/haptic_transformer
 
-nohup python -u transformer_train.py --epochs ${EPOCHS} --batch-size ${BATCH} --num-encoder-layers 2 --nheads 4 --model-type "haptr" --dataset-config-file "/home/mbed/Projects/haptic_transformer/experiments/config/${DATASET}_haptr_12.yaml" >${DATASET}_modoff_light_haptr_12.log &&
-  nohup python -u transformer_train.py --epochs ${EPOCHS} --batch-size ${BATCH} --num-encoder-layers 2 --nheads 4 --model-type "haptr_modatt" --dataset-config-file "/home/mbed/Projects/haptic_transformer/experiments/config/${DATASET}_haptr_12_split.yaml" >${DATASET}_modoff_light_haptr_12_modatt.log &&
-  nohup python -u transformer_train.py --epochs ${EPOCHS} --batch-size ${BATCH} --num-encoder-layers 2 --nheads 4 --model-type "haptr" --dataset-config-file "/home/mbed/Projects/haptic_transformer/experiments/config/${DATASET}_haptr_1_only.yaml" >${DATASET}_modoff_light_haptr_1_only.log &&
+nohup python -u transformer_train.py --epochs ${EPOCHS} --batch-size ${BATCH} --num-encoder-layers 2 --nheads 4 --model-type "haptr" --dataset-config-file "/home/mbed/Projects/haptic_transformer/experiments/config/${DATASET}_haptr_1_only.yaml" >${DATASET}_modoff_light_haptr_1_only.log &&
   nohup python -u transformer_train.py --epochs ${EPOCHS} --batch-size ${BATCH} --num-encoder-layers 2 --nheads 4 --model-type "haptr" --dataset-config-file "/home/mbed/Projects/haptic_transformer/experiments/config/${DATASET}_haptr_2_only.yaml" >${DATASET}_modoff_light_haptr_2_only.log &
