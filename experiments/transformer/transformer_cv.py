36628c3151d7c8e1e1ce56e5d0c31236f7a347c0
mbed92 <michalbednarek.e@gmail.com>
2022-01-05 16:18:34 +0100

    [cross validation script] Added QCAT dataset concatenation method for datasets + improved loading from pickle QCAT

--- experiments/transformer/transformer_cv.py
+++ experiments/transformer/transformer_cv.py
@@ -3,8 +3,9 @@ import os
 
 import numpy as np
 import torch
-import torch.nn as nn
 import yaml
+from sklearn.model_selection import KFold
+from torch import nn
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from torchsummary import summary
@@ -13,8 +14,9 @@ import utils
 from experiments.transformer.transformer_train import accuracy, batch_hits, query, train
 from models import HAPTR, HAPTR_ModAtt
 
-torch.manual_seed(42)
+torch.manual_seed(47)
 DEBUG = False
+FOLDS = 2
 
 
 def main(args):
@@ -26,7 +28,7 @@ def main(args):
         config = yaml.load(file, Loader=yaml.FullLoader)
 
     # load dataset
-    total_dataset, val_ds, test_ds = utils.dataset.load_dataset(config)
+    train_ds, val_ds, test_ds = utils.dataset.load_dataset(config)
 
     # # convert the dataset to pickle for faster loading
     # utils.dataset.save_as_pickle("./qcat.pickle", {
@@ -36,115 +38,117 @@ def main(args):
     # })
 
     # concatenate datasets for cross-validation
-    total_dataset = utils.dataset.concatenate_datasets([total_dataset, val_ds, test_ds])
+    total_dataset = utils.dataset.concatenate_datasets([train_ds, val_ds, test_ds])
     data_shape = total_dataset.signal_length, total_dataset.num_modalities, total_dataset.mean.shape[-1]
-    train_dataloader = DataLoader(total_dataset, batch_size=args.batch_size, shuffle=True)
-    results = {}
 
-    # setup a model
-    if args.model_type == 'haptr_modatt':
-        model = HAPTR_ModAtt(total_dataset.num_classes,
-                             args.projection_dim, total_dataset.signal_length, args.nheads, args.num_encoder_layers,
-                             args.feed_forward, args.dropout, total_dataset.dim_modalities, total_dataset.num_modalities)
-    else:
-        model = HAPTR(total_dataset.num_classes,
-                      args.projection_dim, total_dataset.signal_length, args.nheads, args.num_encoder_layers,
-                      args.feed_forward, args.dropout, total_dataset.dim_modalities, total_dataset.num_modalities)
+    # Define the K-fold Cross Validator
+    kfold = KFold(n_splits=FOLDS, shuffle=True)
 
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model.to(device)
-    summary(model, input_size=data_shape)
+    # K-fold Cross Validation model evaluation
+    for fold, (train_ids, test_ids) in enumerate(kfold.split(total_dataset)):
 
-    # setup optimizer
-    optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.epochs, eta_min=args.eta_min)
-    w = torch.Tensor(total_dataset.weights).to(device)
-    criterion = nn.CrossEntropyLoss(weight=w)
+        # sample dataset for the k-fold
+        train_subsampler = torch.utils.data.SubsetRandomSampler(train_ids)
+        test_subsampler = torch.utils.data.SubsetRandomSampler(test_ids)
 
-    # start
-    best_epoch_accuracy = 0
-    best_acc_test = 0
-    y_pred_val, y_true_val = [], []
-    y_pred_test, y_true_test = [], []
+        # define data loaders for training and testing data in this fold
+        train_dataloader = torch.utils.data.DataLoader(total_dataset, batch_size=args.batch_size,
+                                                       sampler=train_subsampler)
+        test_dataloader = torch.utils.data.DataLoader(total_dataset, batch_size=args.batch_size,
+                                                      sampler=test_subsampler)
 
-    with SummaryWriter(log_dir=log_dir) as writer:
+        # setup a model for the k-th fold
+        if args.model_type == 'haptr_modatt':
+            model = HAPTR_ModAtt(total_dataset.num_classes,
+                                 args.projection_dim, total_dataset.signal_length, args.nheads, args.num_encoder_layers,
+                                 args.feed_forward, args.dropout, total_dataset.dim_modalities,
+                                 total_dataset.num_modalities)
+        else:
+            model = HAPTR(total_dataset.num_classes,
+                          args.projection_dim, total_dataset.signal_length, args.nheads, args.num_encoder_layers,
+                          args.feed_forward, args.dropout, total_dataset.dim_modalities, total_dataset.num_modalities)
 
-        # cross-validation
-        for epoch in range(args.epochs):
-            mean_loss, correct = 0.0, 0
-            model.train(True)
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        model.to(device)
+        summary(model, input_size=data_shape)
 
-            # train loop
-            for step, data in enumerate(train_dataloader):
-                batch_data, batch_labels = utils.dataset.load_samples_to_device(data, device)
-                out, loss = train(batch_data, batch_labels, model, criterion, optimizer)
-                mean_loss += loss.item()
-                correct += batch_hits(out, batch_labels)
+        # setup optimizer
+        optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.epochs, eta_min=args.eta_min)
+        w = torch.Tensor(total_dataset.weights).to(device)
+        criterion = nn.CrossEntropyLoss(weight=w)
 
-            # write to the tensorboard
-            writer.add_scalar('loss/train', mean_loss / len(total_dataset), epoch)
-            writer.add_scalar('accuracy/train', accuracy(correct, len(total_dataset)), epoch)
-            writer.add_scalar('learning_rate', optimizer.param_groups[0]['lr'], epoch)
-            scheduler.step()
+        # start
+        best_epoch_accuracy = 0
+        y_pred_val, y_true_val = [], []
+        results = {}
 
-            # calculate epoch accuracy
-            epoch_accuracy = accuracy(correct, len(val_ds))
-            if epoch_accuracy > best_epoch_accuracy:
-                torch.save(model, os.path.join(writer.log_dir, 'val_model'))
+        # start the cross-validation
+        with SummaryWriter(log_dir=log_dir) as writer:
+            for epoch in range(args.epochs):
+                mean_loss, correct = 0.0, 0
+                model.train(True)
 
-                best_epoch_accuracy = epoch_accuracy
-                results['val'] = best_epoch_accuracy
-                y_pred_val = y_pred
-                y_true_val = y_true
-
-            mean_loss, correct = 0.0, 0
-            model.train(False)
-
-            # run test loop
-            y_pred, y_true = [], []
-            with torch.no_grad():
-                for step, data in enumerate(test_dataloader):
+                # train loop
+                for step, data in enumerate(train_dataloader):
                     batch_data, batch_labels = utils.dataset.load_samples_to_device(data, device)
-                    out, loss = query(batch_data, batch_labels, model, criterion)
+                    out, loss = train(batch_data, batch_labels, model, criterion, optimizer)
                     mean_loss += loss.item()
                     correct += batch_hits(out, batch_labels)
 
-                    # update statistics
-                    _, predicted = torch.max(out.data, 1)
-                    y_pred.extend(predicted.data.cpu().numpy())
-                    y_true.extend(batch_labels.data.cpu().numpy())
+                # write to the tensorboard
+                writer.add_scalar(f'loss_fold_{fold}/train', mean_loss / len(total_dataset), epoch)
+                writer.add_scalar(f'accuracy_{fold}/train', accuracy(correct, len(total_dataset)), epoch)
+                writer.add_scalar(f'learning_rate_{fold}', optimizer.param_groups[0]['lr'], epoch)
+                scheduler.step()
 
-            # calculate epoch accuracy
-            epoch_accuracy = accuracy(correct, len(test_ds))
-            if epoch_accuracy > best_acc_test:
-                torch.save(model, os.path.join(writer.log_dir, 'test_model'))
+                mean_loss, correct = 0.0, 0
+                model.train(False)
 
-                best_acc_test = epoch_accuracy
-                results['test'] = best_acc_test
-                y_pred_test = y_pred
-                y_true_test = y_true
+                # validation loop
+                y_pred, y_true = [], []
+                with torch.no_grad():
+                    for step, data in enumerate(test_dataloader):
+                        batch_data, batch_labels = utils.dataset.load_samples_to_device(data, device)
+                        out, loss = query(batch_data, batch_labels, model, criterion)
+                        mean_loss += loss.item()
+                        correct += batch_hits(out, batch_labels)
 
-                print(f'Epoch {epoch}, test accuracy: {best_acc_test}')
+                        # update statistics
+                        _, predicted = torch.max(out.data, 1)
+                        y_pred.extend(predicted.data.cpu().numpy())
+                        y_true.extend(batch_labels.data.cpu().numpy())
 
-            # update tensorboard
-            writer.add_scalar('loss/test', mean_loss / len(test_ds), epoch)
-            writer.add_scalar('accuracy/test', epoch_accuracy, epoch)
+                # calculate epoch accuracy
+                epoch_accuracy = accuracy(correct, len(val_ds))
+                print(f'Fold {fold}, epoch {epoch}, validation accuracy: {epoch_accuracy}')
 
-        utils.log.save_statistics(y_true_val, y_pred_val, model, os.path.join(log_dir, 'val'), data_shape)
-        utils.log.save_statistics(y_true_test, y_pred_test, model, os.path.join(log_dir, 'test'), data_shape)
-        writer.flush()
+                if epoch_accuracy > best_epoch_accuracy:
+                    torch.save(model, os.path.join(writer.log_dir, f'val_model_fold_{fold}'))
 
-    # save all statistics
-    utils.log.save_dict(results, os.path.join(log_dir, 'results.txt'))
+                    best_epoch_accuracy = epoch_accuracy
+                    results[f'best_val_fold_{fold}'] = best_epoch_accuracy
+                    y_pred_val = y_pred
+                    y_true_val = y_true
 
-    # check performance
-    timings = utils.torch.measure_interence_time(model, args.repetitions, device)
-    results_timer = {
-        "mean": np.sum(timings) / args.repetitions,
-        "std": np.std(timings),
-        "unit": "ms"
-    }
-    utils.log.save_dict(results_timer, os.path.join(log_dir, 'inference_time.txt'))
+                # write to the tensorboard
+                writer.add_scalar(f'loss_{fold}/val', mean_loss / len(val_ds), epoch)
+                writer.add_scalar(f'accurac_{fold}y/val', epoch_accuracy, epoch)
+
+            utils.log.save_statistics(y_true_val, y_pred_val, model, os.path.join(log_dir, f'val_{fold}'), data_shape)
+            writer.flush()
+
+        # save all statistics
+        utils.log.save_dict(results, os.path.join(log_dir, f'results_{fold}.txt'))
+
+        # check performance
+        timings = utils.torch.measure_interence_time(model, args.repetitions, device)
+        results_timer = {
+            "mean": np.sum(timings) / args.repetitions,
+            "std": np.std(timings),
+            "unit": "ms"
+        }
+        utils.log.save_dict(results_timer, os.path.join(log_dir, f'inference_time_fold_{fold}.txt'))
 
 
 if __name__ == '__main__':
